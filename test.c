d1fdea373782839515da4ceba0d59cd08f25bb55
Parna2000 <21ms0093@mc.iitism.ac.in>
2021-09-20 21:54:40 +0530

    dev branch change

---
+++ test.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+    char c='8';
+    int a=c-'0';
+    printf("%d\n",a);
+    int d=2;
+    char b=d+'0';
+    printf("%c\n",b);
+    return 0;
+}
